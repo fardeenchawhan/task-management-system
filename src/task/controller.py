6ea7ac70942245abced051fe66ef909bba89d85c
fardeenchawhan <fardeenchawhan@gmail.com>
2026-06-11 17:46:42 +0530

    creted the get by id endpoint

--- src/task/controller.py
+++ src/task/controller.py
@@ -1,6 +1,7 @@
 from src.task.ditos import TaskSchema
 from sqlalchemy.orm import Session
 from src.task.models import Taskmodel
+from fastapi import HTTPException
 
 def create_task(body:TaskSchema, db:Session):
     data=body.model_dump()
@@ -16,3 +17,9 @@ def create_task(body:TaskSchema, db:Session):
 def get_all_tasks(db:Session):
     tasks=db.query(Taskmodel).all()
     return {"message":"tasks retrieved successfully", "data":tasks}
+
+def get_one_task(task_id:int, db:Session):
+    one_task=db.query(Taskmodel).filter(Taskmodel.id==task_id).first()
+    if not one_task:
+      raise HTTPException(status_code=404, detail="task not found")
+    return {"message":"task retrieved successfully", "data":one_task}
