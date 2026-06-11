a4634f0c38bee6c62433ef0e81409507b04f6d32
fardeenchawhan <fardeenchawhan@gmail.com>
2026-06-11 16:49:33 +0530

    creted the post endpoint

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from src.utils.db import Base,engine
 from src.task.models import Taskmodel
+from src.task.router import task_routes
 
 Base.metadata.create_all(engine)
 app= FastAPI()
+app.include_router(task_routes)
---
+++ src/task/controller.py
@@ -0,0 +1,13 @@
+from src.task.ditos import TaskSchema
+from sqlalchemy.orm import Session
+from src.task.models import Taskmodel
+
+def create_task(body:TaskSchema, db:Session):
+    data=body.model_dump()
+    new_task=Taskmodel(title=data['title'],description=data['description'],is_completed=data['is_completed'])
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+
+
+    return {"message":"task created successfully", "data":new_task}
